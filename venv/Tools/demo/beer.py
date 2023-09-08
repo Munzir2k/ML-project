ff6bda79b805f973d8449f6dc303c71399c47658
Munzir2k <114055530+Munzir2k@users.noreply.github.com>
2023-09-09 00:42:26 +0530

    Add files via upload

--- venv/Tools/demo/beer.py
+++ venv/Tools/demo/beer.py
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ad664a7ea177192866b365f05528d2f67ee85f52da720ff84776dd082122767e
-size 566
+#!/usr/bin/env python3
+
+"""
+A Python version of the classic "bottles of beer on the wall" programming
+example.
+
+By Guido van Rossum, demystified after a version by Fredrik Lundh.
+"""
+
+import sys
+
+n = 100
+if sys.argv[1:]:
+    n = int(sys.argv[1])
+
+def bottle(n):
+    if n == 0: return "no more bottles of beer"
+    if n == 1: return "one bottle of beer"
+    return str(n) + " bottles of beer"
+
+for i in range(n, 0, -1):
+    print(bottle(i), "on the wall,")
+    print(bottle(i) + ".")
+    print("Take one down, pass it around,")
+    print(bottle(i-1), "on the wall.")
--- venv/Tools/pynche/pynche.pyw
+++ venv/Tools/pynche/pynche.pyw
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8b6fbf8816d2c92a2d9ccebb14fbf9d562ed7ea12699b31ab86f97261b7aff3f
-size 181
+#! /usr/bin/env python
+
+"""Run this file under Windows to inhibit the console window.
+Run the file pynche.py under Unix or when debugging under Windows.
+"""
+import Main
+Main.main()
